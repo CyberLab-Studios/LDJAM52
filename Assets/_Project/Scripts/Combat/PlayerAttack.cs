a76c7a9814aa881d8403e71e2f4285b4ace2fc76
R4ndomThunder <samu.pad@hotmail.it>
2023-01-08 22:43:52 +0100

    Update

--- Assets/_Project/Scripts/Combat/PlayerAttack.cs
+++ Assets/_Project/Scripts/Combat/PlayerAttack.cs
@@ -1,3 +1,4 @@
+using CyberLabStudios.Game.Utilities;
 using System;
 using UnityEngine;
 using static UnityEngine.InputSystem.InputAction;
@@ -7,6 +8,8 @@ public class PlayerAttack : MonoBehaviour
     public Animator anim;
     public float weaponCooldown;
     public float animationTime = 3;
+    public AudioData swoosh;
+    public float audioDelay = .5f;
     bool hasScythe = false;
     float tempCooldown;
     bool hasControl = true;
@@ -16,6 +19,7 @@ public class PlayerAttack : MonoBehaviour
         if (ctx.started && tempCooldown <= 0 && hasScythe && hasControl)
         {
             anim.SetTrigger("Attack");
+            Invoke("PlayAudio", audioDelay);
             tempCooldown = weaponCooldown;
             GameEvents.Instance.OnAttack(animationTime);
         }
@@ -43,6 +47,11 @@ public class PlayerAttack : MonoBehaviour
         GameEvents.Instance.onEquipScythe -= GetScytheData;
         GameEvents.Instance.onDialogueStart -= NotControl;
         GameEvents.Instance.onDialogueEnd -= GiveControl;
+    }
+
+    void PlayAudio()
+    {
+        Utility.PlayOneShotAudio(gameObject, swoosh.clip, swoosh.volume);
     }
 
     private void GetScytheData(ScytheData scythe)
